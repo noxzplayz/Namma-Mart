89be445c86b528975ca19eb300adfffa44343755
NOXZPLAYZ <workfornoxzplayz@gmail.com>
2025-06-10 08:21:20 +0530

    Delete backend directory

--- backend/index.js
+++
@@ -1,5 +0,0 @@
-// Basic Express setup
-const express = require('express'); const app = express(); app.use(express.json());
-const products = require('./routes/products'); const orders = require('./routes/orders');
-app.use('/products', products); app.use('/order', orders); app.use('/orders', orders);
-app.listen(process.env.PORT || 3000);
